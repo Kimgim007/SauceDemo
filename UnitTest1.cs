052c2f060d01080a98428a977c30f378bf4b90e2
maks.kep.2000@mail.ru <maks.kep.2000@mail.ru>
2024-06-12 18:03:23 +0300

    Add project files.

---
+++ UnitTest1.cs
@@ -0,0 +1,21 @@
+namespace SauceDemo
+{
+    public class Tests
+    {
+        [SetUp]
+        public void Setup()
+        {
+        }
+
+        [Test]
+        public void Test1()
+        {
+            Assert.Pass();
+        }
+        [TearDown]
+        public void TearDow()
+        {
+
+        }
+    }
+}
